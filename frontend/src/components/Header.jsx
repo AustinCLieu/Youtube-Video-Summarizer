79900f887a40a4bfe64041759e03116c65768109
AustinCLieu <asianonrice@gmail.com>
2025-08-27 15:00:55 -0700

    Removed default Vite layout and added Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <h1>
+                    <span>🎬</span>
+                    Your Social Media Video Summarizer
+                </h1>
+                <p>Transform any social media video into clear, concise summaries of your preferred language</p>
+            </div>
+        </header>
+    )
+}
