bd78762db9512a5e41461ef27ca7489df2b172a9
AustinCLieu <asianonrice@gmail.com>
2025-08-27 17:02:53 -0700

    Added VideoInput, Virtual Environment, FastAPI, requirements

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI #webframework for create API server
+from pydantic import BaseModel #used to define and validate structured data(JSON)
+from fastapi.middleware.cors import CORSMiddleware #allows frontend(React) to run on a different port to communicate with backend safely
+
+##Instantiates my API application
+app = FastAPI() 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#Creates a pydantic model to validate incoming JSON
+class VideoRequest(BaseModel):
+    url: str
+
+@app.post("/summarize") #defines an endpoint for POST requests at /summarize
+def summarize_video(request: VideoRequest): #automatically parses the incoming JSON into a Python object)
+    return {"summary": f"Summary of video at {request.url}"} #FastAPI converts the Python dictionary to JSON and sends it back to the frontend
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI backend is running!"}
--- frontend/src/components/VideoInput.jsx
+++ frontend/src/components/VideoInput.jsx
@@ -7,20 +7,46 @@ export default function VideoInput() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        try {
+            const res = await fetch("http://localhost:8000/summarize", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ url: videoLink }),
+            });
+
+            const data = await res.json();
+            setResponse(data.summary);
+        } catch (err) {
+            console.error(err);
+            setResponse("Something went wrong. Please try again");
+        }
     }
 
     return (
-        <section>
-            <h2>Enter Video Link</h2>
-            <p>Paste a link from TikTok, Instagram, YouTube, or any other social media platform </p>
-            <form onSubmit={handleSubmit}>
-                <input type="url"
-                    placeholder="Paste url here"
-                    value={videoLink}
-                    onChange={(e) => setVideoLink(e.target.value)}
-                />
-                <button type="submit">Summarize</button>
-            </form>
-        </section>
+        <div>
+            <section>
+                <h2>Enter Video Link</h2>
+                <p>Paste a link from TikTok, Instagram, YouTube, or any other social media platform </p>
+                <form onSubmit={handleSubmit}>
+                    <input type="url"
+                        placeholder="Paste url here"
+                        value={videoLink}
+                        onChange={(e) => setVideoLink(e.target.value)}
+                    />
+                    <button type="submit">Summarize</button>
+                </form>
+            </section>
+
+            <section>
+                {response && (
+                    <div>
+                        <p>Summary</p>
+                        <p>{response}</p>
+                    </div>
+                )}
+            </section>
+        </div>
+
     )
 }
